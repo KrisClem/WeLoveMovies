97811ee26bacbca406772644c8295191ffaec012
KrisClem <71985892+KrisClem@users.noreply.github.com>
2021-10-07 16:21:58 -0700

    fixed spelling error

--- src/db/migrations/20211005193417_createMoviesTheatersTable.js
+++ src/db/migrations/20211005193417_createMoviesTheatersTable.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("movies_theaters", (table) => {
       table.integer("movie_id").references("movie_id").inTable("movies");
-      table.integer("theater_id").references("theater_id").inTable("theater");
+      table.integer("theater_id").references("theater_id").inTable("theaters");
       table.boolean("is_showing");
   })
 };
